534fbc22e2c4c371c8bd774aecc173afeed44723
vicic8484 <77392126+vicic8484@users.noreply.github.com>
2023-07-20 21:26:33 +0200

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -8,7 +8,7 @@
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        GM_xmlhttpRequest
-// @connect      github.com
+// @connect      raw.githubusercontent.com
 // ==/UserScript==
 
 (() => {
